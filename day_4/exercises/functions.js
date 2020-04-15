a8f3243e0f9d6ebd094334bd7d204cb7332c7363
Marcos Chavez <marcoschavez45@yahoo.com>
2020-04-14 19:52:48 -0600

    Add day 4 work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -13,18 +13,29 @@ printName();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
-
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName() {
+  return printName = "Marcos Chavez";
 };
 
-printName("Albus Dumbledore");
-
+console.log(printName);
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+var x = 33;
+var y = 55;
 
+function sum() {
+  return x + y;
+};
+
+console.log(sum());
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+var jedi = "The defenders of Light, ";
+var sith = "agents of chaos.";
+
+function starWars() {
+  return jedi + sith;
+ };
+
+console.log(starWars());
